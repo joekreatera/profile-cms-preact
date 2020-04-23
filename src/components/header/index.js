f8e6b03221bab6f774fb3f102a96f667a796c5cc
Joemx <jose.c.mondragon@gmail.com>
2020-04-23 15:41:56 -0500

    Changed format and colors

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,13 +3,20 @@ import { Link } from 'preact-router/match';
 import style from './style';
 
 const Header = () => (
-	<header class={style.header}>
-		<Link href="/"><h1>Joe Mondragon</h1></Link>
-		<nav>
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
-			<Link activeClassName={style.active} href="/contact">Contact me</Link>
-		</nav>
+	<header class="">
+		<div class="container">
+			<div class="jumbotron">
+				<Link href="/"><h1>Joe Mondragon</h1></Link>
+				<p>
+					<nav>
+						<Link activeClassName={style.active} href="/blogs">Blogs</Link>
+						<Link activeClassName={style.active} href="/contact">Contact me</Link>
+					</nav>
+				</p>
+			</div>
+		</div>
 	</header>
+
 );
 
 export default Header;
