892e2045bbf650df3e9faae6facd521e97665bbb
Joemx <jose.c.mondragon@gmail.com>
2020-04-25 07:59:27 -0500

    updated style and links

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,23 +5,25 @@ import style from './style';
 const Header = () => (
 
 	<header class="">
-		<div class="container">
-			<div class="row">
-			&nbsp;
-			</div>
+
+	<div class="container">
+		<div class="row">
+				&nbsp;
 		</div>
-	
-		<div class="container">
-			<div class="jumbotron">
-				<Link href="/"><h1>Joe Mondragon</h1></Link>
-				<p>
-					<nav>
-						<Link activeClassName={style.active} href="/blogs">Blogs</Link>
-						<Link activeClassName={style.active} href="/contact">Contact me</Link>
-					</nav>
-				</p>
-			</div>
+
+		<div class="jumbotron">
+					<h1>Welcome to the projects showcase</h1>
+					<p>
+						<nav>
+							<Link activeClassName={style.active} href="http://joe.videogamesacademy.com">Back</Link>
+							|
+							<Link activeClassName={style.active} href="/contact">Contact me</Link>
+						</nav>
+					</p>
 		</div>
+
+	</div>
+
 	</header>
 
 );
